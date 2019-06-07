83ca4f1c4a60203fa6acf0bbcc9072e39c0f18d2
Ellen Kidane <ellen.kidane@rallyhealth.com>
2019-06-07 14:29:38 -0500

    Update files in ui folder to incorporate routing (as completed so far)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Home extends Component {
 
@@ -11,13 +12,48 @@ class Home extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // read in user input from text box
   handleChange(e) {
     this.setState({ input: e.target.value });
   };
 
   handleClick() {
-    var inputAsList = (this.state.input).split(',');
-    this.setState({ inputList: inputAsList });
+    var inputAsList = (this.state.input).split(','); // get each rallyID from the user input
+    var userList = inputAsList.map(item =>  // make list of rallyIDs
+        ({id: item, successful: 'default = failed reset, probably waiting for response'}));
+    this.setState({ inputList: userList });
+
+    const newUserList = [];
+
+    // for each rallyID
+    for (const item in userList) {
+        const IDasString = (userList[item].id).toString()
+        // call curl command to reset their data
+        axios.get("http://localhost:9000/callCurl/" + IDasString)
+            .then( response => {
+                console.log(response.data)
+                // get the stdout from the curl command (will contain error message if one occurred)
+                var responseSplit = response.data.toString().split(";")
+                var stdoutString = responseSplit[1]
+                // update the new list with whether or not reset was successful
+                if (response.data.includes("successful")){
+                    newUserList.push(
+                        {id: userList[item].id,  successful: 'successful reset' }
+                     );
+                } else {
+                    newUserList.push(
+                        {id: userList[item].id,  successful: 'failed reset - ' + stdoutString }
+                    );
+                }
+                this.setState({ inputList: newUserList});
+            })
+            // if there is an error with the request, print using console.log()
+            .catch((error) => {
+                console.log('error: ' + error);
+             })
+
+    }
+
   }
 
   render() {
@@ -37,26 +73,19 @@ class Home extends Component {
 
         { /* place to input the rallyid(s) */ }
         <p>Enter rallyIDs below (separate by commas please - but no spaces):</p>
+        { /* as user inputs rallyID(s) into the text box, they are read in and saved */}
         <textarea value={this.state.input} onChange={this.handleChange} rows="25" cols="170"/>
+        {/* and then when this button is clicked, the curl command to reset their data is called */}
         <div> <button onClick={this.handleClick}>Reset These Users</button> </div>
 
         { /* place to display the results */ }
         <div>
             <br/>
-            <p>Sample results:</p>
+            <p>Results: </p>
             <ul style={{border: '0.5px solid black', paddingBottom : '5px', width: '1000px'}}>
-                <li>rallyID here - successful reset</li>
-                <li>another rallyID - failed reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - failed reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - successful reset</li>
-                <li>another rallyID - successful reset</li>
-                { this.state.inputList.map(item => (
-                    <li>{item}</li>
+                { /* display rallyID and whether or not reset was successful */ }
+                { this.state.inputList.map( (item, index) => (
+                    <li key={index}>{item.id} - {item.successful}</li>
                 ))}
             </ul>
         </div>
@@ -64,6 +93,7 @@ class Home extends Component {
       </div>
     );
   }
+
 }
 
 export default Home;
